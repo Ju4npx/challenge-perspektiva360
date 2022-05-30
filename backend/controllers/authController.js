3ae6916451095cfbfe23e870dafb3931e43327c6
Juan Pablo Machado <juanpmachadob@gmail.com>
2022-05-29 22:57:00 -0500

    feat(backend): Add user login function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,8 @@ const generateJWT = require("../helpers/jwt");
 const User = require("../models/User");
 
 const register = async (req, res) => {
-  const { firstName, lastName, telephone, email, password, gender, city } = req.body;
+  const { firstName, lastName, telephone, email, password, gender, city } =
+    req.body;
   try {
     const user = new User({
       firstName,
@@ -22,7 +23,44 @@ const register = async (req, res) => {
     await user.save();
 
     // Generate JWT token
-    const token = await generateJWT(user.id);
+    const token = await generateJWT(user.id, user.name);
+
+    return res.json({
+      ok: true,
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Please, contact the administrator",
+    });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // Verify if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "User not found",
+      });
+    }
+
+    // Verify if passwords match
+    const validPassword = bcryptjs.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Invalid password",
+      });
+    }
+
+    const token = await generateJWT(user.id, user.name);
 
     return res.json({
       ok: true,
@@ -40,4 +78,5 @@ const register = async (req, res) => {
 
 module.exports = {
   register,
+  login,
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,11 +5,13 @@
 
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { register } = require("../controllers/authController");
+const { register, login } = require("../controllers/authController");
 const validateFields = require("../middlewares/validateFields");
 
 const router = Router();
 
 router.post("/register", [], register);
+
+router.post("/login", [], login);
 
 module.exports = router;
