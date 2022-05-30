dd4b856652eca45e7d178afb16805fc6f16f8684
Juan Pablo Machado <juanpmachadob@gmail.com>
2022-05-30 00:16:27 -0500

    refactor(backend): change auth response value _id to id

--- backend/models/User.js
+++ backend/models/User.js
@@ -35,7 +35,8 @@ const UserSchema = Schema({
 });
 
 UserSchema.methods.toJSON = function () {
-  const { __v, password, ...user } = this.toObject();
+  const { __v, _id, password, ...user } = this.toObject();
+  user.id = _id;
   return user;
 };
 
